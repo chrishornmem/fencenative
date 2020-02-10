4dd9e44ccd288b26ed8542b267f9148207161cde
Chris Horn <chrishornuk@gmail.com>
2020-02-10 15:28:38 +0000

    updating readme

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'fencenative',         # How you named your package folder (MyLib)
   packages = ['fencenative'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Superfences custom formatter for mkdocs for using native html such as img tag',   # Give a short description about your library
   author = 'Chris Horn',                   # Type in your name
   author_email = 'chris.horn@onem.com',      # Type in your E-Mail
   url = 'https://github.com/chrishornmem/fencenative.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/chrishornmem/fencenative/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/chrishornmem/fencenative/archive/0.2.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
